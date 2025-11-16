98c91d2e6bf98c6549b49f2a3458200b3d18cddf
Kyle Stoltzfus <computerwhiz33@gmail.com>
2025-11-16 01:06:33 -0500

    Display line graph for cash flow analytics

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -5,37 +5,47 @@ import { useState } from "react";
 import dayjs, { Dayjs } from "dayjs";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import Stack from "@mui/material/Stack";
+import useSWR from "swr";
+import { getAnalyticsData } from "@/lib/analytics";
+import CashFlowGraph from "@/components/CashFlowGraph";
 
 export default function Analytics() {
   const [startDate, setStartDate] = useState<Dayjs | null>(dayjs());
-
   const [endDate, setEndDate] = useState<Dayjs | null>(dayjs());
 
+  const { data, isLoading, mutate } = useSWR(
+    ["/analytics", startDate?.format("MM-YYYY"), endDate?.format("MM-YYYY")],
+    getAnalyticsData,
+  );
+
   return (
-    <Stack
-      spacing={2}
-      justifyContent={"center"}
-      direction={"row"}
-      sx={{ m: 2 }}
-    >
+    <>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DatePicker
-          label="Start Date"
-          views={["year", "month"]}
-          format="MM/YYYY"
-          value={startDate}
-          onChange={(newValue) => setStartDate(newValue)}
+        <Stack
+          spacing={2}
+          justifyContent={"center"}
+          direction={"row"}
           sx={{ m: 2 }}
-        />
-        <DatePicker
-          label="End Date"
-          views={["year", "month"]}
-          format="MM/YYYY"
-          value={endDate}
-          onChange={(newValue) => setEndDate(newValue)}
-          sx={{ m: 2 }}
-        />
+        >
+          <DatePicker
+            label="Start Date"
+            views={["year", "month"]}
+            format="MM/YYYY"
+            value={startDate}
+            onChange={(newValue) => setStartDate(newValue)}
+            sx={{ m: 2 }}
+          />
+          <DatePicker
+            label="End Date"
+            views={["year", "month"]}
+            format="MM/YYYY"
+            value={endDate}
+            onChange={(newValue) => setEndDate(newValue)}
+            sx={{ m: 2 }}
+          />
+        </Stack>
       </LocalizationProvider>
-    </Stack>
+      <CashFlowGraph budgets={data} />
+    </>
   );
 }
---
+++ components/CashFlowGraph.tsx
@@ -0,0 +1,61 @@
+import { BudgetSummary } from "@/types/api";
+import { LineChart } from "@mui/x-charts";
+import { formatToCurrency } from "@/lib/utils";
+
+interface CashFlowGraphProps {
+  budgets?: BudgetSummary[] | undefined;
+}
+
+export default function CashFlowGraph({ budgets }: CashFlowGraphProps) {
+  let totalExpenses = 0;
+  let totalIncome = 0;
+  const chartData =
+    budgets
+      ?.sort((a, b) => {
+        if (a.year !== b.year) {
+          return a.year - b.year;
+        }
+        return a.month - b.month;
+      })
+      .map((budget) => {
+        totalExpenses += budget.actualExpenses;
+        totalIncome += budget.actualIncome;
+        return {
+          x: { month: budget.month, year: budget.year },
+          actualIncome: totalIncome,
+          actualExpenses: totalExpenses,
+        };
+      }) ?? [];
+
+  return (
+    <LineChart
+      dataset={chartData}
+      xAxis={[
+        {
+          scaleType: "point",
+          dataKey: "x",
+          valueFormatter: (value) => `${value.month}/${value.year}`,
+        },
+      ]}
+      yAxis={[
+        {
+          width: 75,
+          valueFormatter: (value: number | null) => formatToCurrency(value),
+        },
+      ]}
+      series={[
+        {
+          dataKey: "actualIncome",
+          label: "Income",
+          valueFormatter: (value: number | null) => formatToCurrency(value),
+        },
+        {
+          dataKey: "actualExpenses",
+          label: "Expenses",
+          valueFormatter: (value: number | null) => formatToCurrency(value),
+        },
+      ]}
+      height={300}
+    />
+  );
+}
